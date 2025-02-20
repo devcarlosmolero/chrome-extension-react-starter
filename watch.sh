8dcb85c472e75c56b38ed2b52a73f34b2e60ca6c
Carlos Molero <163675207+devcarlosmolero@users.noreply.github.com>
2025-02-20 01:54:28 +0100

    update: watch.sh

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 while true; do
-  inotifywait -r -e modify,create,delete ./chrome-extension-starter-react-dist
-  cp -r ./chrome-extension-starter-react-dist /media/sf_GUEST/
+  inotifywait -r -e modify,create,delete ./chrome-extension-react-starter-dist
+  cp -r ./chrome-extension-react-starter-dist /media/sf_GUEST/
 done
